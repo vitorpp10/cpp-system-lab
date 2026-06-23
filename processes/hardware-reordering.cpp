7841f52f53924fbc244d3a0b29ae05406a16d762
vitor <vitorprofissionalpp10@gmail.com>
2026-06-23 12:19:46 -0300

    Create hardware-reordering.cpp

---
+++ processes/hardware-reordering.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#include<thread>
+
+#define ll long long
+#define fastio \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL)
+
+#define endl "\n"
+
+using namespace std;
+
+int x = 0;
+int y = 0;
+int r1 = 0;
+int r2 = 0;
+
+void t1() {
+  x = 1;
+  asm volatile("" ::: "memory");
+  r1 = y;
+}
+
+void t2() {
+  y = 1;
+  asm volatile("" ::: "memory");
+  r2 = x;
+}
+
+int main() {
+    fastio;
+    int d = 0;
+    for(int i = 0; i < 1'000'000; i++) {
+      x = 0, y = 0, r1 = 0, r2 = 0;
+      thread ta(t1);
+      thread tb(t2);
+      if(ta.joinable()) ta.join();
+      if(tb.joinable()) tb.join();
+      if(r1 == 0 && r2 == 0) d++;
+    }
+    cout << "how much times hardware lier compiler and volatile: " << d << endl;
+    return 0;
+}
